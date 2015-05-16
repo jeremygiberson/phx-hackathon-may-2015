0bbf5715d28a95e90cba4cb50a981585e2737cfd
Jeremy Giberson <jeremyg@webpt.com>
2015-05-16 13:50:48 -0700

    added fk on delete cascade

---
+++ hackathon/module/Application/src/Application/Service/RemindMe/DbAdapterRemindMe.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace Application\Service\RemindMe;
+
+
+use Application\Service\CollectionDays\CollectionDay;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\Adapter\AdapterAwareInterface;
+use Zend\Db\TableGateway\TableGateway;
+
+class DbAdapterRemindMe implements RemindMeInterface, AdapterAwareInterface
+{
+    /** @var  Adapter */
+    protected $adapter;
+
+    /**
+     * @param string $email
+     * @param string $address
+     * @param CollectionDay[] $days
+     */
+    public function subscribe($email, $address, $days)
+    {
+        $subscription = new TableGateway('subscription', $this->adapter);
+        $id = $subscription->insert([
+            'email' => $email,
+            'address' => $address
+        ]);
+
+        foreach($days as $day)
+        {
+            $reminder = new TableGateway('reminder', $this->adapter);
+            $reminder->insert([
+                'subscription_id' => $id,
+                'day' => $day->getDay(),
+                'service' => $day->getService()
+            ]);
+        }
+    }
+
+    /**
+     * @param string $address
+     * @param string $email
+     */
+    public function unsubscribe($address, $email)
+    {
+        // TODO: Implement unsubscribe() method.
+    }
+
+    /**
+     * Set db adapter
+     *
+     * @param Adapter $adapter
+     * @return AdapterAwareInterface
+     */
+    public function setDbAdapter(Adapter $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+}
